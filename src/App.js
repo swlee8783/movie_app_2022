0224b6b31ca1a5e56bd04131f00d13d8b1869ed5
swlee8783 <siwon8783@gmail.com>
2022-11-09 01:17:34 +0100

    CheckPropTypes 활용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import CheckPropTypes from 'prop-types';
 
-function App() {
+function Food({name, picture, rating}){
+  //console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
+  
   );
 }
+
+const foodILike = [
+  {
+    id : 1,
+    name : 'Kimchi',
+    image : 'https://img-cf.kurly.com/shop/data/goodsview/20201214/gv40000142772_1.jpg',
+    rating : 5,
+  },
+  {
+    id : 2,
+    name : 'Samgiopsal',
+    image : 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg',
+    rating : 4.9,
+  },
+  {
+    id : 3,
+    name : 'Bibimbap',
+    image : 'https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/S9Y/image/76pr0EfQi6R21MMIyzefN9hiqIU.jpg',
+    rating : 3.8,
+  },
+  {
+    id : 4,
+    name : 'Kimbap',
+    image : 'https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG',
+    rating : 4.1,
+  },
+];
+
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image} />;
+// }
+
+function App() {
+  // return <div className="App" />;
+  return (
+  <div>
+    <h1>Hello</h1>
+    {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
+  </div>
+  );
+}
+
+Food.CheckPropTypes = {
+  name: CheckPropTypes.string.isRequired,
+  picture: CheckPropTypes.string.isRequired,
+  rating: CheckPropTypes.number.isRequired,
+};
 
 export default App;
